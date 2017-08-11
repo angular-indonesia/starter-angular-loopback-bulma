9a2cdd59d5427dc727b6bf003bfe7de8c58d57cb
Julian GM Alimin <dmastag@yahoo.com>
2017-08-11 16:33:45 +0700

    Adding Login, Home and Dashboard Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { DashboardPageComponent } from './home-page/dashboard-page/dashboard-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent,
+    DashboardPageComponent
   ],
   imports: [
     BrowserModule,
