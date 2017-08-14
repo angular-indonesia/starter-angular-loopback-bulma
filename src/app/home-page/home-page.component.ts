48f1aed2b86803ea95846d470303e28e272dba96
Dzulfaqar <dzulfaqar@dataon.com>
2017-08-14 15:44:29 +0700

    add listener for toggle button in small layout

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -10,6 +10,16 @@ export class HomePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.toggleMenu();
+  }
+
+  toggleMenu(){
+    var burger = document.querySelector('.navbar-burger');
+    var menu = document.querySelector('.nav-menu');
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
   }
 
 }
