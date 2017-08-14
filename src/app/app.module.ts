367eea9e1fcfb0c315094be448483291ffb2af94
INDAH-NB\INDAH <indahrahma0909@gmail.com>
2017-08-14 15:29:12 +0700

    import FileUploadPage Component to App.Modele.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { DashboardPageComponent } from './home-page/dashboard-page/dashboard-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
+import { FileUploadPageComponent } from './file-upload-page/file-upload-page.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { SignupPageComponent } from './signup-page/signup-page.component';
     LoginPageComponent,
     HomePageComponent,
     DashboardPageComponent,
-    SignupPageComponent
+    SignupPageComponent,
+    FileUploadPageComponent
   ],
   imports: [
     BrowserModule,
