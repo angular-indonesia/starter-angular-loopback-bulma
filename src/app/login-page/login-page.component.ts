77825b3b40c17288e894fdb48057586fcf852f0f
Fuguh Budi Utomo <Odin@Odin-MacBook-Pro.local>
2017-08-14 17:44:48 +0700

    add login logic

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,10 @@
+import { DashboardPageComponent } from './../home-page/dashboard-page/dashboard-page.component';
+import { UserApi } from './../../shared/services/custom/User';
 import { Component, OnInit } from '@angular/core';
+import { NgModel, FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +12,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  public username: any;
+  public password: any;
+  constructor(
+  
+  public userApi : UserApi,
 
-  constructor() { }
+  ) { }
 
   ngOnInit() {
+  }
+
+  doLogin() {
+    console.log(this.username,this.password);
+    const data = {
+      username: this.username,
+      password: this.password
+    };
+    this.userApi.login(data)
+      .subscribe(() => {
+        console.log("sukses");
+      }, (error) => {
+        console.log(error);
+      });
   }
 
 }
