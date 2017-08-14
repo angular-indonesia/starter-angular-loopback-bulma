e3234423c6540cf9bbca04aa17e739bc1d533a69
INDAH-NB\INDAH <indahrahma0909@gmail.com>
2017-08-14 15:16:50 +0700

    add pages file upload

---
+++ src/app/file-upload-page/file-upload-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './file-upload-page.component.html',
+  styleUrls: ['./file-upload-page.component.scss']
+})
+export class FileUploadPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
