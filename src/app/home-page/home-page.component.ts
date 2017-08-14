3dc54466d48fac28b188bae3935e77ffc2be03a7
Dzulfaqar <dzulfaqar@dataon.com>
2017-08-14 17:27:50 +0700

    add variable for toggle condition

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomePageComponent implements OnInit {
 
   constructor() { }
+  showMiniMenu: boolean = false;
 
   ngOnInit() {
     this.toggleMenu();
