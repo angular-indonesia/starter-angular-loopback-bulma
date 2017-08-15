cf58a17998698e73608b1c889d5e90e85536f254
Dzulfaqar <dzulfaqar@dataon.com>
2017-08-15 13:17:41 +0700

    standarize the code format

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomePageComponent implements OnInit {
 
   showMiniMenu: boolean = false;
+  showMiniMenu: Boolean = false;
 
   constructor() {
   }
