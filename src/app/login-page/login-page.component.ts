82f32e6dda8720c318feb3fc9b8f9fcd07d6d6f1
Rendy Sutandy <rendy.sutandy@dataon.com>
2017-08-15 19:08:15 +0700

    remove whitespace tslint

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -5,7 +5,6 @@ import { Component, OnInit } from '@angular/core';
 import { NgModel, FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Router }    from '@angular/router';
 
 
 @Component({
