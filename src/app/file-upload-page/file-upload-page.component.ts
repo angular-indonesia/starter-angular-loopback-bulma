175ceb38221ec9035d7817f93f99a9c7abb2c7c8
INDAH-NB\INDAH <indahrahma0909@gmail.com>
2017-08-16 13:55:16 +0700

    add function create container and upload

--- src/app/file-upload-page/file-upload-page.component.ts
+++ src/app/file-upload-page/file-upload-page.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 import { StorageSimpleUploadApi } from './../../shared/services/custom/StorageSimpleUpload';
 import { LoopBackConfig } from './../../shared/lb.config';
+import { NgModel, FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './file-upload-page.component.html',
   styleUrls: ['./file-upload-page.component.scss']
 })
+
 export class FileUploadPageComponent implements OnInit {
   public nameSimpleFile: any;
   public titleOriginalFile: any;
-  public loopbackPath: string = LoopBackConfig.getPath();
+  public loopbackPath: string = LoopBackConfig.getPath()+"/api/StorageSimpleUploads/";
   public evenPhoto: any;
+  public name_container: any = "";
+  public dataPhoto: any;
+  public urlSimpleDownload: any = "";
+   public urlSimpleUpload: any = "";
   constructor(
-   
+    public storagesimpleuploadapi: StorageSimpleUploadApi
   ) {
     this.nameSimpleFile = "";
     this.titleOriginalFile = "Name File";
@@ -20,9 +27,7 @@ export class FileUploadPageComponent implements OnInit {
 
   ngOnInit() {
   }
-  uploadSimpleFile(event) {
-
-
+  addFile(event) {
     let name = event.path[0].files[0].name;
     let fileIMG = ['png', 'jpg', 'jpeg', 'bmp', 'gif'];
     let formatImage = name.split('.').pop();
@@ -32,25 +37,56 @@ export class FileUploadPageComponent implements OnInit {
     this.nameSimpleFile = options;
     this.titleOriginalFile = name;
     this.evenPhoto = event;
-
   }
+
   upload() {
     if (this.nameSimpleFile == "") {
       console.log("Sorry Files Is Empty");
     } else {
-      let filesToUpload = <Array<File>>this.evenPhoto.target.files;
-      console.log("test" + this.evenPhoto.path[0].files[0].name);
-      let urlSimpleUpload = this.loopbackPath + "/api/StorageSimpleUploads/profile/upload";
-      this.makeFileRequest(urlSimpleUpload, [], filesToUpload, this.nameSimpleFile).then((result) => {
-        console.log(JSON.stringify(result));
+      this.createRequestFile("profile");
 
-      }, (error) => {
-        console.error(error);
-
-      });
     }
   }
 
+  createAndUpload() {
+    if (this.nameSimpleFile == "" && this.name_container == "") {
+      console.log("Sorry Files Is Empty");
+    } else {
+      console.log(this.name_container);
+      let data = {
+        "name": "" + this.name_container.toLowerCase()
+      }
+      this.storagesimpleuploadapi.createContainer(data)
+        .subscribe(() => {
+          this.createRequestFile(this.name_container);
+        }, (error) => {
+          if (error.status == 500) {
+            this.createRequestFile(this.name_container);
+          } else {
+            console.log(error);
+          }
+
+        });
+    }
+  }
+
+  createRequestFile(nameContainer) {
+    let filesToUpload = <Array<File>>this.evenPhoto.target.files;
+    console.log("test" + this.evenPhoto.path[0].files[0].name);
+    this.urlSimpleUpload = this.loopbackPath + "" + nameContainer + "/upload";
+    this.makeFileRequest(this.urlSimpleUpload, [], filesToUpload, this.nameSimpleFile).then((result) => {
+      this.dataPhoto = {
+        "nameFile": result['result'].files.file[0].name,
+        "nameContainer": result['result'].files.file[0].container
+      }
+      
+      this.urlSimpleDownload = this.loopbackPath +""+this.dataPhoto.nameContainer+"/download/"+this.dataPhoto.nameFile;
+      console.log(this.urlSimpleDownload);
+    }, (error) => {
+      console.error(error);
+
+    });
+  }
   makeFileRequest(url: string, params: Array<string>, files: Array<File>, options) {
     return new Promise((resolve, reject) => {
       const formData: any = new FormData();
