fc5f0f97aa75c23d05b5024f5def16dafa06534e
Fuguh Budi Utomo <Odin@Odin-MacBook-Pro.local>
2017-08-18 18:05:42 +0700

    add maps to sdk model

--- src/shared/services/custom/index.ts
+++ src/shared/services/custom/index.ts
@@ -1,6 +1,7 @@
 /* tslint:disable */
 export * from './UserPhoto';
 export * from './UserCredential';
+export * from './StorageSimpleUpload';
+export * from './Maps';
 export * from './SDKModels';
 export * from './logger.service';
-export * from './StorageSimpleUpload';
