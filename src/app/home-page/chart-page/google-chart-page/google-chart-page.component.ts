c3b1d0184608e1e86af7c41db183e052543b41a3
Dzulfaqar <dzulfaqar@dataon.com>
2017-08-21 20:43:44 +0700

    delete unused comment in GoogleChartPageComponent.ts

--- src/app/home-page/chart-page/google-chart-page/google-chart-page.component.ts
+++ src/app/home-page/chart-page/google-chart-page/google-chart-page.component.ts
@@ -6,22 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./google-chart-page.component.scss']
 })
 export class GoogleChartPageComponent implements OnInit {
-  // public pie_ChartData = [ ['Task', 'Hours per Day'], ['Work', 11], ['Eat', 2], ['Commute', 2], ['Watch TV', 2], ['Sleep', 7] ];
-  // public pie_ChartOptions = { title: 'My Daily Activities', width: 400, height: 400 };
-  // public line_ChartData = [
-  //   ['Year', 'Sales', 'Expenses'],
-  //   ['2004', 1000, 400],
-  //   ['2005', 1170, 460],
-  //   ['2006', 660, 1120],
-  //   ['2007', 1030, 540]];
 
-  // public line_ChartOptions = {
-  //   title: 'Company Performance',
-  //   curveType: 'function',
-  //   legend: {
-  //     position: 'bottom'
-  //   }
-  // };
   constructor() { }
 
   ngOnInit() {
