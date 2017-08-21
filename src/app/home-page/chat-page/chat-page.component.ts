0f6a864adb5c900e958ccf0fc7216ad145451501
Luthfir <luthfirrahman04@gmail.com>
2017-08-21 15:32:47 +0700

    adding room chat api

--- src/app/home-page/chat-page/chat-page.component.ts
+++ src/app/home-page/chat-page/chat-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from '../../chat.service';
 import { Router } from '@angular/router';
+import { RoomchatApi } from '../../../shared/services/custom/Roomchat';
 
 @Component({
   selector: 'app-chat-page',
@@ -9,22 +10,29 @@ import { Router } from '@angular/router';
 })
 export class ChatPageComponent implements OnInit {
 
-  chats:any[];
+  chats: any[];
+  roomChat: any;
   constructor(
     private chatService: ChatService,
-    private router: Router) {
+    private router: Router,
+    private roomChatApi: RoomchatApi) {
 
-    
+
+      this.getRoom();
   }
 
   ngOnInit() {
     this.chats = this.chatService.getChats();
   }
 
-  onSelect(id){
-    console.log(id);
-    
+  onSelect(id) {
     this.router.navigate(['/home/chat/chatroom/', id]);
+  }
+
+  getRoom() {
+    this.roomChatApi.find().subscribe((result) => {
+      this.roomChat = result
+    })
   }
 
 }
