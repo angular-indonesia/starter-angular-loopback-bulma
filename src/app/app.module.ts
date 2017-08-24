0af475e58b15e447d8315bbc87d7b51cd259633e
Rendy Sutandy <rendy.sutandy@dataon.com>
2017-08-24 16:25:58 +0700

    add page 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -56,6 +56,7 @@ export function highchartsFactory() {
     GoogleChartPageComponent,
     HighchartsPageComponent,
     FileUploadMasterPageComponent,
+    NotFoundPageComponent,
   ],
   imports: [
     BrowserModule,
