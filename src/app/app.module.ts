47533d631849dc1f8c0164fa1e313e375199825d
INDAH-NB\INDAH <indahrahma0909@gmail.com>
2017-08-24 09:37:05 +0700

    add module file master page di app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,17 +13,18 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { DashboardPageComponent } from './home-page/dashboard-page/dashboard-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
 import { ChartPageComponent } from './home-page/chart-page/chart-page.component';
-import { FileUploadPageComponent } from './file-upload-page/file-upload-page.component';
+import { FileUploadPageComponent } from './file-upload-master-page/file-upload-page/file-upload-page.component';
 import { ChatPageComponent } from './home-page/chat-page/chat-page.component';
 import { ChatRoomPageComponent } from './home-page/chat-room-page/chat-room-page.component';
-import { FileUploadAdvancedPageComponent } from './file-upload-advance-page/file-upload-advance-page.component';
+import { FileUploadAdvancedPageComponent } from './file-upload-master-page/file-upload-advance-page/file-upload-advance-page.component';
 import { CrudPageComponent } from './home-page/crud-page/crud-page.component';
 import { MapsPageComponent } from './maps-page/maps-page.component';
 import { AgmCoreModule, MapsAPILoader } from '@agm/core';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { Ng2chartPageComponent } from './home-page/chart-page/ng2chart-page/ng2chart-page.component';
 import { GoogleChartPageComponent } from './home-page/chart-page/google-chart-page/google-chart-page.component';
-import { HighchartsPageComponent } from './home-page/chart-page/highcharts-page/highcharts-page.component';
+import { HighchartsPageComponent } from './home-page/chart-page/highcharts-page/highcharts-page.component'
+import { FileUploadMasterPageComponent } from './file-upload-master-page/file-upload-master-page.component';
 
 @NgModule({
   declarations: [
@@ -44,6 +45,7 @@ import { HighchartsPageComponent } from './home-page/chart-page/highcharts-page/
     Ng2chartPageComponent,
     GoogleChartPageComponent,
     HighchartsPageComponent,
+    FileUploadMasterPageComponent
   ],
   imports: [
     BrowserModule,
