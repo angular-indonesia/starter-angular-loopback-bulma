5c1e9c296c4b8e7bed399312427599cfca0c4fdb
dimas.prasetya <dimas.prasetya@dataon.com>
2017-08-30 16:09:19 +0700

    add chat realtime

--- src/app/home-page/chat-room-page/chat-room-page.component.ts
+++ src/app/home-page/chat-room-page/chat-room-page.component.ts
@@ -3,7 +3,12 @@ import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { ChatService } from '../../chat.service';
 import { RoomchatApi } from '../../../shared/services/custom/Roomchat';
 import { Http, Response } from '@angular/http';
+import { ChatdetailApi } from '../../../shared/services/custom/Chatdetail';
+import { RealTime } from '../../../shared/services/core/real.time';
+import { FireLoopRef } from '../../../shared/models/FireLoopRef';
+import { Chatdetail } from '../../../shared/models/Chatdetail';
 import 'rxjs/add/operator/switchMap';
+
 @Component({
   selector: 'app-chat-room-page',
   templateUrl: './chat-room-page.component.html',
@@ -11,15 +16,28 @@ import 'rxjs/add/operator/switchMap';
 })
 export class ChatRoomPageComponent implements OnInit {
 
-  chats: any;
-  id: number;
+  public chats: any;
+  public id: number;
+  public chatDetail: Chatdetail = new Chatdetail();
+  public todoRef: FireLoopRef<Chatdetail>;
+
   constructor(
     private route: ActivatedRoute,
     private chatService: ChatService,
     private router: Router,
-    private roomChatApi: RoomchatApi
+    private roomChatApi: RoomchatApi,
+    private chatDetailApi: ChatdetailApi,
+    private rt: RealTime
   ) {
-    this.getChatDetail();
+
+    this.rt.onReady().subscribe((status: string) => {
+      this.todoRef = this.rt.FireLoop.ref<Chatdetail>(Chatdetail);
+      this.todoRef.on('change').subscribe((chats: any) => {
+        console.log(chats, 'isi chats');
+        this.chats = chats;
+      });
+      this.getChatDetail();
+    });
   }
 
 
@@ -28,17 +46,23 @@ export class ChatRoomPageComponent implements OnInit {
 
   getChatDetail() {
     this.route.params.forEach(params => {
-      this.roomChatApi.find({
+      // this.roomChatApi.find({
+      //   where: {
+      //     id: this.route.snapshot.params['id']
+      //   }
+      // }).subscribe((result) => {
+      //   this.chats = result;
+      // });
+
+      this.chatDetailApi.find({
         where: {
           id: this.route.snapshot.params['id']
         }
       }).subscribe((result) => {
         this.chats = result;
-      })
+      });
     });
 
   }
-
-
 
 }
